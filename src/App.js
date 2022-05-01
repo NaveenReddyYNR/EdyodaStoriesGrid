ca1b0d844b930bf46cc0798848494b013b89edfe
NaveenReddyYNR <100398391+NaveenReddyYNR@users.noreply.github.com>
2022-05-01 17:09:29 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import "./edyoda-stories.css";
+import "./App.css";
+import Header from "./component/Header";
+import Body from "./component/Body";
+import Footer from "./component/Footer";
+<script
+  src="https://kit.fontawesome.com/df73c9e721.js"
+  crossorigin="anonymous"
+></script>;
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Body />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
